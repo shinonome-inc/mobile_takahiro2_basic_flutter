c2e7ba7b87df86eca778eeecd105e2ea8ca1e8a9
taka392 <gongguyiji@gmail.com>
2023-05-20 00:40:50 +0900

    fix

--- qiita_app/lib/main.dart
+++ qiita_app/lib/main.dart
@@ -12,70 +12,88 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body:Stack(
+        body: Stack(
           children: [
             Container(
-            decoration: const BoxDecoration(
-            image: DecorationImage(
-            image: AssetImage('images/background.png'),
-            fit: BoxFit.cover,
-            ),),
-                child:  Center(
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.2),
+                image: const DecorationImage(
+                  image: AssetImage('images/background.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Center(
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.fromLTRB(0, 250, 0, 0),
-                        child: Text('Qiita Feed App',
-                        style: TextStyle(
-                          color: Colors.white, // テキストの色を白に設定
-                          fontSize: 40, // テキストのフォントサイズを24に設定
-                          fontFamily: 'Pacifico',
-                        ),
-                        ),
-                      ),
-                      const Text('-PlayGround-',
-                        style: TextStyle(
-                          color: Colors.white, // テキストの色を白に設定
-                          fontSize: 13, // テキストのフォントサイズを24に設定
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 400, 0, 0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: const Color.fromRGBO(70,131,1,1),
-                            borderRadius: BorderRadius.circular(20.0),
+                children: [
+                  const SizedBox(
+                    height: 250,
+                  ),
+                  const Text(
+                    'Qiita Feed App',
+                    style: TextStyle(
+                      color: Colors.white, // テキストの色を白に設定
+                      fontSize: 40, // テキストのフォントサイズを40に設定
+                      fontFamily: 'Pacifico',
+                    ),
+                  ),
+                  const Text(
+                    '-PlayGround-',
+                    style: TextStyle(
+                      color: Colors.white, // テキストの色を白に設定
+                      fontSize: 13, // テキストのフォントサイズを13に設定
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            // ボタンが押されたときの処理
+                          },
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white, backgroundColor: const Color.fromRGBO(70, 131, 1, 1),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 14.0, horizontal: 130.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
                           ),
-                          child: const Padding(
-                            padding: EdgeInsets.symmetric(vertical: 14.0, horizontal: 130.0),
-                            child: Text('ログイン',
-                              style: TextStyle(
-                                color: Colors.white, // テキストの色を白に設定
-                                fontSize: 15, // テキストのフォントサイズを24に設定
-                                fontWeight: FontWeight.bold,
-                              ),
+                          child: const Text(
+                            'ログイン',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
                         ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.all(40.0),
-                        child: Text('ログインせずに利用する',
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  TextButton(
+                      onPressed: (){
+                        //クリックされた時の処理
+                  },
+                      child: const Text('ログインせずに利用する',
                         style: TextStyle(
                           color: Colors.white, // テキストの色を白に設定
-                          fontSize: 15, // テキストのフォントサイズを24に設定
+                          fontSize: 15, // テキストのフォントサイズを15に設定
                           fontWeight: FontWeight.bold,
                         ),
-                        ),
-                      ),
-                    ],
-                  )
-                ),
-            ),
-            Container(
-              color: Colors.black.withOpacity(0.2), // 黒色の背景に透明度20%を設定
+                      ),),
+                  const SizedBox(
+                    height: 70,
+                  ),
+                ],
+              )),
             ),
           ],
         ),
@@ -83,4 +101,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
