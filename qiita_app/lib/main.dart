9e60200b62464acdc57a60a54360dc1b8064899c
taka392 <gongguyiji@gmail.com>
2023-05-20 17:19:43 +0900

    5_20_17

--- qiita_app/lib/main.dart
+++ qiita_app/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+<<<<<<< HEAD
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -23,11 +24,117 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+=======
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/background.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            ColorFiltered(
+              colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.2), BlendMode.dstATop),
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 6,
+                    child: Container(
+                    ),
+                  ),
+                  const Text(
+                    'Qiita Feed App',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontFamily: 'Pacifico',
+                    ),
+                  ),
+                  const Text(
+                    '-PlayGround-',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 8,
+                    child: Container(
+                    ),
+                  ),
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            // ボタンが押されたときの処理
+                          },
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: const Color.fromRGBO(70, 131, 1, 1),
+                            padding: const EdgeInsets.symmetric(vertical: 14.0, horizontal: 130.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                          ),
+                          child: const Text(
+                            'ログイン',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // クリックされた時の処理
+                    },
+                    child: const Text(
+                      'ログインせずに利用する',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+>>>>>>> ced6e74 (add_flex)
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+<<<<<<< HEAD
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -113,3 +220,5 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+=======
+>>>>>>> ced6e74 (add_flex)
