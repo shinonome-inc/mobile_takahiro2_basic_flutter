5c53f0fc56f7a917b6067a23ede5f5c675b84461
taka392 <gongguyiji@gmail.com>
2023-06-29 20:21:14 +0900

    qiita_auth_key

---
+++ qiita_app/lib/qiita_auth_key.dart
@@ -0,0 +1,8 @@
+// MobileQiitaAppで認証を行う際に利用するClientIDとClientSecretを格納するファイルです。
+// このファイル（qiita_auth_key.dart）はセキュリティの観点より、Gitの管理下から外しています。
+// 利用する際は必ずmobile_qiita_app/lib/qiita_auth_key.dartに挿入してください。
+
+class QiitaAuthKey {
+  static const String clientId = 'Your clientId';
+  static const String clientSecret = 'Your clientSecret';
+}
