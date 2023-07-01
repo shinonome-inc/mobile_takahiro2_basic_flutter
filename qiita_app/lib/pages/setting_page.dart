82a198ee8bd7bc303f2f04d09562fcaf93254a46
taka392 <gongguyiji@gmail.com>
2023-07-01 22:43:50 +0900

    setting

--- qiita_app/lib/pages/setting_page.dart
+++ qiita_app/lib/pages/setting_page.dart
@@ -26,16 +26,176 @@ class _SettingPageState extends State<SettingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const DefaultAppBar(text: 'Setting'),
-      body: Center(
-        child: Column(
-          children: [
-            Column(
-              children: [
-                Container()
-              ],
+      body: Container(
+        color: const Color(0xFFF2F2F2),
+        child: ListView(
+          children: <Widget>[
+            const SizedBox(
+              height: 32,
             ),
+            const ListTile(
+              title: Text('アプリ情報',
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
+              )
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                debugPrint("タップされました");
+              },
+                child: Container(
+                  color: Colors.white,
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(16, 8.0,8.0, 0),
+                        height: 40,
+                        child: const Text(
+                              'プライバシーポリシー',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                        ),
+
+                      ),
+                      const Icon(
+                        Icons.arrow_forward_ios_outlined,
+                        color: Colors.grey
+                      ),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                    ],
+                  ),
+                ),
+            ),
+            GestureDetector(
+              onTap: () {
+                debugPrint("タップされました");
+              },
+              child: Container(
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(16, 8.0,8.0, 0),
+                      height: 40,
+                      child: const Text(
+                        '利用規約',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                      ),
+
+                    ),
+                    const Icon(
+                        Icons.arrow_forward_ios_outlined,
+                        color: Colors.grey
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                debugPrint("タップされました");
+              },
+              child: Container(
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(16, 8.0,8.0, 0),
+                      height: 40,
+                      child: const Text(
+                        'アプリケーションバージョン',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                      ),
+
+                    ),
+                    const Icon(
+                        Icons.arrow_forward_ios_outlined,
+                        color: Colors.grey
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const ListTile(
+              title: Text('その他',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 12,
+                  )
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                QiitaClient.deleteAccessToken();
+              },
+              child: Container(
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(16, 8.0,8.0, 0),
+                      height: 40,
+                      child: const Text(
+                        'ログアウトする',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                      ),
+
+                    ),
+                    const Icon(
+                        Icons.arrow_forward_ios_outlined,
+                        color: Colors.grey
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(child: Container(),),
+            // 他のリストアイテムを追加できます
           ],
-        )
+        ),
       )
     );
   }
