6441137d5dc7583f009dbe77832fbe3536e36edb
taka392 <gongguyiji@gmail.com>
2023-07-01 23:02:07 +0900

    delete_clientId

--- qiita_app/lib/qiita_auth_key.dart
+++ qiita_app/lib/qiita_auth_key.dart
@@ -3,6 +3,6 @@
 // 利用する際は必ずmobile_qiita_app/lib/qiita_auth_key.dartに挿入してください。
 
 class QiitaAuthKey {
-  static const String clientId = '145998f67ac78db6453572c3d8522cab2a8fb4c0';
-  static const String clientSecret = '6bd33756702ff37b1ac02b314d0e75063fc4a566';
+  static const String clientId = '';
+  static const String clientSecret = '';
 }
