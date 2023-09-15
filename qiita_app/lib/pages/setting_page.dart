ce7c5d7a23a172ec11d1e05c2f2a83dc5cdc8494
taka392 <gongguyiji@gmail.com>
2023-09-15 13:47:34 +0900

    アプリ情報の配置を修正しました

--- qiita_app/lib/pages/setting_page.dart
+++ qiita_app/lib/pages/setting_page.dart
@@ -7,8 +7,6 @@ import 'package:qiita_app/models/texts.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:qiita_app/services/repository.dart';
 
-
-
 class SettingPage extends StatefulWidget {
   const SettingPage({Key? key}) : super(key: key);
 
@@ -17,13 +15,12 @@ class SettingPage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<SettingPage> {
-
   String? accessToken;
   bool isNoLogin = false;
 
-  Future<void>checkAccessToken()async{
+  Future<void> checkAccessToken() async {
     accessToken = await QiitaClient.getAccessToken();
-    if(accessToken==null){
+    if (accessToken == null) {
       setState(() => isNoLogin = true);
     }
   }
@@ -33,13 +30,12 @@ class _SettingPageState extends State<SettingPage> {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) {
-        return TopPage(redirecturl: '',);
+        return TopPage(
+          redirecturl: '',
+        );
       }),
     );
   }
-
-
-
 
   @override
   void initState() {
@@ -47,219 +43,218 @@ class _SettingPageState extends State<SettingPage> {
     checkAccessToken();
     getAppVersion();
   }
+
   Future<String> getAppVersion() async {
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
     return packageInfo.version;
   }
+
   void deleteCurrentAccessToken() async {
     await QiitaClient.deleteAccessToken();
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(
-          builder: (_) => TopPage(redirecturl: 'https://',)),
+          builder: (_) => TopPage(
+                redirecturl: 'https://',
+              )),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: const DefaultAppBar(text: 'Setting'),
-        body: Container(
-          color: const Color(0xFFF2F2F2),
-          child: ListView(
-            children: <Widget>[
-              Container(
-                height: 32,
+      appBar: const DefaultAppBar(text: 'Setting'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            height: 32,
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(16, 0, 0, 8),
+            child: Text(
+              "アプリ情報",
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
               ),
-              const ListTile(
-                title: Text('アプリ情報',
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              showModalBottomSheet<void>(
+                context: context,
+                backgroundColor: Colors.transparent,
+                isScrollControlled: true,
+                builder: (BuildContext context) {
+                  return const TextSizedBox(
+                    privacyPolicyText: Texts.privacyPolicy,
+                    appBarTitle: 'プライバシーポリシー',
+                  );
+                },
+              );
+            },
+            child: Container(
+              color: Colors.white,
+              child: Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(16, 8.0, 8.0, 0),
+                    height: 40,
+                    child: const Text(
+                      'プライバシーポリシー',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const Expanded(
+                    child: SizedBox(),
+                  ),
+                  const Icon(Icons.arrow_forward_ios_outlined,
+                      color: Colors.grey),
+                  Container(
+                    width: 16,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              showModalBottomSheet<void>(
+                context: context,
+                backgroundColor: Colors.transparent,
+                isScrollControlled: true,
+                builder: (BuildContext context) {
+                  return const TextSizedBox(
+                    privacyPolicyText: Texts.termOfServiceText,
+                    appBarTitle: '利用規約',
+                  );
+                },
+              );
+            },
+            child: Container(
+              color: Colors.white,
+              child: Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(16, 8.0, 8.0, 0),
+                    height: 40,
+                    child: const Text(
+                      '利用規約',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const Expanded(
+                    child: SizedBox(),
+                  ),
+                  const Icon(Icons.arrow_forward_ios_outlined,
+                      color: Colors.grey),
+                  Container(
+                    width: 16,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            child: Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.fromLTRB(16, 8.0, 8.0, 0),
+                  height: 40,
+                  child: const Text(
+                    'アプリケーションバージョン',
                     style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.grey,
-                    )
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  showModalBottomSheet<void>(
-                    context: context,
-                    backgroundColor: Colors.transparent,
-                    isScrollControlled: true,
-                    builder: (BuildContext context) {
-                      return const TextSizedBox(
-                        privacyPolicyText: Texts.privacyPolicy,
-                        appBarTitle: 'プライバシーポリシー',
-                      );
-                    },
-                  );
-                },
-                child: Container(
-                  color: Colors.white,
-                  child: Row(
+                const Expanded(
+                  child: SizedBox(),
+                ),
+                FutureBuilder<String>(
+                  future: getAppVersion(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Text('v ${snapshot.data}',
+                          style: const TextStyle(fontSize: 14));
+                    } else if (snapshot.hasError) {
+                      return Text('Error: ${snapshot.error}');
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  },
+                ),
+                Container(
+                  width: 16,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 20,
+          ),
+          Container(
+            child: isNoLogin
+                ? Container()
+                : Column(
                     children: [
-                      Container(
-                        padding: const EdgeInsets.fromLTRB(16, 8.0,8.0, 0),
-                        height: 40,
-                        child: const Text(
-                          'プライバシーポリシー',
+                      const ListTile(
+                        title: Text(
+                          'その他',
                           style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
+                            color: Colors.grey,
+                            fontSize: 12,
                           ),
                         ),
                       ),
-                      const Expanded(
-                        child: SizedBox(
-                        ),
-                      ),
-                      const Icon(
-                          Icons.arrow_forward_ios_outlined,
-                          color: Colors.grey
-                      ),
-                      Container(
-                        width: 16,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  showModalBottomSheet<void>(
-                    context: context,
-                    backgroundColor: Colors.transparent,
-                    isScrollControlled: true,
-                    builder: (BuildContext context) {
-                      return const TextSizedBox(
-                        privacyPolicyText: Texts.termOfServiceText,
-                        appBarTitle: '利用規約',
-                      );
-                    },
-                  );
-                },
-                child: Container(
-                  color: Colors.white,
-                  child: Row(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.fromLTRB(16, 8.0,8.0, 0),
-                        height: 40,
-                        child: const Text(
-                          '利用規約',
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      const Expanded(
-                        child: SizedBox(
-                        ),
-                      ),
-                      const Icon(
-                          Icons.arrow_forward_ios_outlined,
-                          color: Colors.grey
-                      ),
-                      Container(
-                        width: 16,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            Container(
-        color: Colors.white,
-        child: Row(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(16, 8.0,8.0, 0),
-                      height: 40,
-                      child: const Text(
-                        'アプリケーションバージョン',
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    const Expanded(
-                      child: SizedBox(
-                      ),
-                    ),
-                    FutureBuilder<String>(
-                      future: getAppVersion(),
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          return Text('v ${snapshot.data}',style: const TextStyle(fontSize: 14));
-                        } else if (snapshot.hasError) {
-                          return Text('Error: ${snapshot.error}');
-                        } else {
-                          return const CircularProgressIndicator();
-                        }
-                      },
-                    ),
-                    Container(
-                      width: 16,
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                height: 20,
-              ),
-              Container(
-                child: isNoLogin
-                    ? Container()
-                    : Column(
-                  children: [
-                    const ListTile(
-                      title: Text(
-                        'その他',
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        deleteCurrentAccessToken();
-                      },
-                      child: Container(
-                        color: Colors.white,
-                        child: Row(
-                          children: [
-                            Container(
-                              padding: const EdgeInsets.fromLTRB(16, 8.0, 8, 0),
-                              height: 40,
-                              child: const Text(
-                                'ログアウトする',
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
+                      GestureDetector(
+                        onTap: () {
+                          deleteCurrentAccessToken();
+                        },
+                        child: Container(
+                          color: Colors.white,
+                          child: Row(
+                            children: [
+                              Container(
+                                padding:
+                                    const EdgeInsets.fromLTRB(16, 8.0, 8, 0),
+                                height: 40,
+                                child: const Text(
+                                  'ログアウトする',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
                               ),
-                            ),
-                            const Expanded(
-                              child: SizedBox(
+                              const Expanded(
+                                child: SizedBox(),
                               ),
-                            ),
-                            const Icon(
-                              Icons.arrow_forward_ios_outlined,
-                              color: Colors.grey,
-                            ),
-                            Container(
-                              width: 16,
-                            ),
-                          ],
+                              const Icon(
+                                Icons.arrow_forward_ios_outlined,
+                                color: Colors.grey,
+                              ),
+                              Container(
+                                width: 16,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+                    ],
+                  ),
           ),
-        )
+        ],
+      ),
     );
   }
 }
